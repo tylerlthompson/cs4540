b115a6f351a7023c20fb6cd9c8915fb439a6d4c5
Tyler Thompson <tylerthompson1993@gmail.com>
2019-10-06 00:20:03 -0400

    a3 working

--- A3/source_multiprocessed.c
+++ A3/source_multiprocessed.c
@@ -1,101 +1,79 @@
-
+/**
+* Tyler Thompson
+* WMU CS4540 - A3
+* Oct 8, 2019
+*/
 #include <stdio.h>  
 #include <string.h>
 #include <stdlib.h> 
-#include <fcntl.h>           /* For O_* constants */
-#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>
 #include <semaphore.h>
-#include <sys/types.h>
+#include <time.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
-#include <errno.h>
+#include <signal.h>
 
 void Process(sem_t * screen, sem_t * keyboard, int index);
 int getSemaphores(sem_t * screen, sem_t * keyboard, int index);
+void handle_sigterm();
+
 
 int main(int argc, char * argv[]) {
-
-//   sem_open both semaphores
-
-//   fork 7 processes 
-// 	Each process passed index of loop so 1,2,3, or 4
-//   Below is real C to show just how easy this is, please do not overthink
-
     sem_t * sems, * semk;
-    sems = sem_open("/sems", O_CREAT, 0644, 1);
-    semk = sem_open("/semk", O_CREAT, 0644, 1);
+    int pids[7]; 
+    int pid, i = 0, j = 0;
 
-    int pids[7]; int pid;
-    int i = 0;
-    int child_in[2];
-    pipe(child_in);
+    // sem_open both semaphores
+    sems = sem_open("/sems", O_CREAT, 0600, 1);
+    semk = sem_open("/semk", O_CREAT, 0600, 1);
+
     do {
-        
-        if( (pid = fork()) == 0) {
-            
-            dup2(child_in[0], 0);
-
-            close(child_in[1]);
+        if( (pid = fork()) == 0) {  // fork 7 processes
             Process(sems, semk, i+1);
         }
         else {
             pids[i] = pid;
             i++;
-            int status;
-            waitpid(pid, &status, WNOHANG);
-            dup2(child_in[1], 1);
-            close(child_in[0]);
         }
-        //printf("Pid: %d Index: %d\n", pid, i);
     } while( i < 7 && pid > 0);
 
     if(i >= 7) { // stop forking around
+        signal(SIGTERM, handle_sigterm);
+        signal(SIGINT, handle_sigterm);
         // after all die, use single call or loop based on ids saved above
+        for(j=0; j<7; j++) waitpid(pids[j], NULL, 0);
+
         // sem_unlink both
-        
         sem_unlink("/sems");
         sem_unlink("/semk");
-
-        // dup2(child_in[0], 0);
-        // close(child_in[0]);
-        // close(child_in[1]);
 
         printf("Done.\n");
     }
     return 0;
 }   
 
+void handle_sigterm() {
+    sem_unlink("/sems");
+    sem_unlink("/semk");
+    printf("Unlinking semaphores and terminating Pid: %d\n", getpid());
+    exit(0);
+}
 
 void Process (sem_t * screen, sem_t * keyboard, int index) {
-    // sem_open both semaphores if necessary
-    //screen = sem_open("/sems", O_CREAT, 0644, 0);
-    //keyboard = sem_open("/semk", O_CREAT, 0644, 0);
-
-    // count = 0
-    int count = 0;
+    int count = 0, status, id = getpid();
     char input[80];
- 	int id = getpid();
-    //printf("Processing %d\n", id);
+
+    // sem_open both semaphores if necessary
+    if (sem_getvalue(keyboard, &status) == -1) keyboard = sem_open("/semk", O_CREAT, 0600, 0);
+    if (sem_getvalue(screen, &status) == -1) screen = sem_open("/sems", O_CREAT, 0600, 0);
     
     do { //	loop
-        //printf("Pid: %d Count: %d\n", id, count);
-        // count += getSemaphores(index)
         count += getSemaphores(screen, keyboard, index);
-        printf("Pid: %d Count: %d\n", id, count);
 
         // prompt id + " enter < 80 characters " + id + " or q to quit: "
         printf("%d enter < 80 characters %d or q to quit\n", id, id);
-        //sleep(10);
-        // read keboard
-        // if (fgets(input, 80, stdin) == 0) {
-        //     printf("Failed to read input on pid %d", id);
-        //     exit(0);
-        // }
-        getc(stdin);
         fscanf(stdin, " %s", input);
 
-        //printf("StRCMP: %d\n", strcmp(input, "q"));
         // echo what was typed with id before and after echo
         // echo id + " start " + what was typed + id + " end"
         printf("%d start >%s< %d end\n", id, input, id);
@@ -106,11 +84,10 @@ void Process (sem_t * screen, sem_t * keyboard, int index) {
 
     } while (strcmp(input, "q") != 0); // until quit end inner loop in C do ... while not q
 
-    // count += getSemaphores(index)
     count += getSemaphores(screen, keyboard, index);
 
     // prompt "This process " + id + " had " + count + " deadlocks "
-    printf("This process %d has %d deadlocks", id, count);
+    printf("This process %d had %d deadlocks\n", id, count);
 
     // sem_post or give back both semaphores
     sem_post(screen);
@@ -120,20 +97,14 @@ void Process (sem_t * screen, sem_t * keyboard, int index) {
     sem_close(screen);
     sem_close(keyboard);
 
-// exit
-exit(0);
+    // exit
+    exit(0);
 }
 
 // function to get semaphores (screen, keyboard, index)
 int getSemaphores(sem_t * screen, sem_t * keyboard, int index) {
     // returns count of how many times recovered from deadlock
-    // count = 0
-    int count = 0;
-    int have_both = 0;
-    int semk_value = 0;
-    int sems_value = 0;
-    int timed_wait = 0;
-
+    int count = 0, have_both = 0;
     struct timespec ts;
 
     // can use temp and swap semphores
@@ -150,38 +121,28 @@ int getSemaphores(sem_t * screen, sem_t * keyboard, int index) {
     }
 
     do { // loop 
-        
         // sem_wait for first
         sem_wait(*first_sem);
 
         // sleep(1) // only use this if no deadlocks without it should not be necessary
-        sleep(1);
+        if (count == 0 ) sleep(1);
 
         // sem_timedwait with timer for second
-        ts.tv_sec = 3;
-        ts.tv_nsec = 0;
-        timed_wait = sem_timedwait(*second_sem, &ts);
-        if (timed_wait != 0) { // if timeout 
+        ts.tv_sec = 0;
+        ts.tv_nsec = 10000;
+        if (sem_timedwait(*second_sem, &ts) != 0) { // if timeout 
             
             // give back first, (sem_post)
             sem_post(*first_sem);
             // wait random time
-            sleep(rand() % 3);
+            usleep((rand() % 3) * 1000000);
             // increment count
             count++;
         }
         else { // else
             // have both  true   
             have_both = 1;
-            printf("Now we have both\n");
         }
-
-        sem_getvalue(keyboard, &semk_value);
-        sem_getvalue(screen, &sems_value);
-        //printf("Pid: %d Have Both: %d Count: %d Index: %d Semk: %d Sems: %d TimedWait: %d ErrorNo %d\n", getpid(), have_both, count, index, semk_value, sems_value, timed_wait, errno);
     } while(have_both == 0); // until have both
-
-//  return count
     return count;
-// end get semaphores function
-}
+} // end get semaphores function
---
+++ A3/source_threaded.c
@@ -0,0 +1,173 @@
+/**
+* Tyler Thompson
+* WMU CS4540 - A3
+* Oct 8, 2019
+*/
+#include <stdio.h>  
+#include <string.h>
+#include <stdlib.h> 
+#include <semaphore.h>
+#include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
+
+#define NUM_THREADS 7
+
+void * Process(void * args_t);
+int getSemaphores(sem_t * screen, sem_t * keyboard, int index);
+void handle_sigterm();
+
+struct arg_struct {
+    sem_t * screen;
+    sem_t * keyboard;
+    int index;
+    int thread_complete;
+};
+
+int main(int argc, char * argv[]) {
+
+    sem_t sems, semk;
+    pthread_t threads[NUM_THREADS];
+    struct arg_struct thread_args[NUM_THREADS];
+    int i, done_threads = 0;
+
+    // sem_open both semaphores
+    sem_init(&sems, 0, 1);
+    sem_init(&semk, 0, 1);
+
+    // register signal hanglers
+    signal(SIGTERM, handle_sigterm);
+    signal(SIGINT, handle_sigterm);
+
+    // create all threads
+    for (i=0; i<NUM_THREADS; i++) {
+        thread_args[i].keyboard = &semk;
+        thread_args[i].screen = &sems;
+        thread_args[i].index = i;
+        thread_args[i].thread_complete = 0;
+        pthread_create(&(threads[i]), NULL, &Process, (void *) &(thread_args[i]));
+    }
+
+    // wait until all threads are done
+    while(done_threads != NUM_THREADS) {
+        done_threads = 0;
+        for (i=0; i<NUM_THREADS; i++) {
+            if (thread_args[i].thread_complete == 1) {
+                done_threads++;
+            }
+        }
+        sleep(1);
+    }
+
+    // join all threads
+    for (i=0; i<NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Done.\n");
+    return 0;
+}   
+
+void handle_sigterm() {
+    sem_unlink("/sems");
+    sem_unlink("/semk");
+    printf("Unlinking semaphores and terminating Pid: %d\n", getpid());
+    exit(0);
+}
+
+void * Process (void * args_t) {
+    int status, count = 0;
+    char input[80];
+    struct arg_struct * args = args_t;
+
+    // sem_open both semaphores if necessary
+    if (sem_getvalue(args->keyboard, &status) == -1) sem_init(args->keyboard, 0, 1);
+    if (sem_getvalue(args->screen, &status) == -1) sem_init(args->screen, 0, 1);
+    
+    do { //	loop
+        count += getSemaphores(args->screen, args->keyboard, args->index);
+
+        // prompt id + " enter < 80 characters " + id + " or q to quit: "
+        printf("%d enter < 80 characters %d or q to quit\n", args->index, args->index);
+        fscanf(stdin, " %s", input);
+
+        // echo what was typed with id before and after echo
+        // echo id + " start " + what was typed + id + " end"
+        printf("%d start >%s< %d end\n", args->index, input, args->index);
+
+        // sem_post or give back both semaphores
+        sem_post(args->screen);
+        sem_post(args->keyboard);
+
+    } while (strcmp(input, "q") != 0); // until quit end inner loop in C do ... while not q
+
+    count += getSemaphores(args->screen, args->keyboard, args->index);
+
+    // prompt "This process " + id + " had " + count + " deadlocks "
+    printf("This thread %d had %d deadlocks\n", args->index, count);
+
+    // sem_post or give back both semaphores
+    sem_post(args->screen);
+    sem_post(args->keyboard);
+
+    // sem_close 
+    sem_close(args->screen);
+    sem_close(args->keyboard);
+
+    // exit
+    args->thread_complete = 1;
+    pthread_exit(0);
+}
+
+// function to get semaphores (screen, keyboard, index)
+int getSemaphores(sem_t * screen, sem_t * keyboard, int index) {
+    // returns count of how many times recovered from deadlock
+
+    int count = 0, have_both = 0, semk_value = 0, sems_value = 0, timed_wait = 0;
+    struct timespec ts;
+
+    // can use temp and swap semphores
+    sem_t * * first_sem;
+    sem_t * * second_sem;
+    if (index % 2 == 0) { // odd index gets screen first
+        first_sem = &screen;
+        second_sem = &keyboard;
+    } 
+    else { // even index gets keyboard first
+        first_sem = &keyboard;
+        second_sem = &screen;
+    }
+
+    do { // loop 
+        
+        // sem_wait for first
+        sem_wait(*first_sem);
+
+        // sleep(1) // only use this if no deadlocks without it should not be necessary
+        if (count == 0 ) sleep(1);
+
+        // sem_timedwait with timer for second
+        ts.tv_sec = 0;
+        ts.tv_nsec = 10000;
+        timed_wait = sem_timedwait(*second_sem, &ts);
+        if (timed_wait != 0) { // if timeout 
+            
+            // give back first, (sem_post)
+            sem_post(*first_sem);
+            // wait random time
+            usleep((rand() % 3) * 1000000);
+            // increment count
+            count++;
+        }
+        else { // else
+            // have both  true   
+            have_both = 1;
+        }
+
+        sem_getvalue(keyboard, &semk_value);
+        sem_getvalue(screen, &sems_value);
+    } while(have_both == 0); // until have both
+
+    return count;
+} // end get semaphores function
